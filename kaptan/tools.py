fd7ab69f57a659baca76f32aceae32099f1daf6d
mthnzbk <mthnzbk@gmail.com>
2016-02-24 22:32:53 +0200

    fonksiyonlar ilgili dosyaya yanlış ve defalarca aynı bilgiyi yazıyordu fixlendi. Duvar kağıdı yanlış yazılıyordu bu yüzden ilgili fonksiyonlar yazıldı.

--- kaptan/tools.py
+++ kaptan/tools.py
@@ -5,8 +5,28 @@ MidButton;NoModifier=org.kde.paste
 RightButton;NoModifier=org.kde.contextmenu
 wheel:Vertical;NoModifier=org.kde.switchdesktop
 
-[AppletGlobals][org.kde.plasma.notifications]
-popupPosition=0
+[ActionPlugins][1]
+RightButton;NoModifier=org.kde.contextmenu
+
+[ActionPlugins][127]
+RightButton;NoModifier=org.kde.contextmenu
+
+[Containments]
+AppletOrder=2
+Image=file:///usr/share/wallpapers/Kite/contents/images/800x600.jpg
+activityId=782e3974-9f83-40bd-b1a4-165f60c3aa75
+extraItems=org.kde.plasma.volume, org.kde.plasma.notifications, org.kde.discovernotifier, org.kde.plasma.mediacontroller, org.kde.plasma.battery, org.kde.plasma.bluetooth, org.kde.plasma.clipboard, org.kde.plasma.networkmanagement, org.kde.plasma.devicenotifier
+favorites=preferred://browser, systemsettings.desktop, org.kde.dolphin.desktop, org.kde.kate.desktop
+formfactor=0
+global=Alt+F1
+height=768
+immutability=1
+knownItems=org.kde.plasma.volume, org.kde.plasma.notifications, org.kde.discovernotifier, org.kde.plasma.mediacontroller, org.kde.plasma.battery, org.kde.plasma.bluetooth, org.kde.plasma.clipboard, org.kde.plasma.networkmanagement, org.kde.plasma.devicenotifier
+lastScreen=0
+location=0
+plugin=org.kde.plasma.desktop
+wallpaperplugin=org.kde.image
+width=1366
 
 [Containments][1]
 activityId=
@@ -17,54 +37,110 @@ location=4
 plugin=org.kde.panel
 wallpaperplugin=org.kde.image
 
-[Containments][1][Applets][31][Configuration][General]
-favoriteApps=preferred://browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop
-
-[Containments][1][Applets][32]
+[Containments][1][Applets][2]
 immutability=1
 plugin=org.kde.plasma.kickoff
 
-[Containments][1][Applets][32][Configuration][General]
-favoriteApps=preferred://browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop
+[Containments][1][Applets][2][Configuration][General]
+favorites=preferred://browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop
 
-[Containments][38]
-activityId=1073333c-eb01-4786-b734-55fd1a3be375
+[Containments][1][Applets][2][Configuration][Shortcuts]
+global=Alt+F1
+
+[Containments][1][Applets][2][Shortcuts]
+global=Alt+F1
+
+[Containments][1][Applets][3]
+immutability=1
+plugin=org.kde.plasma.pager
+
+[Containments][1][Applets][4]
+immutability=1
+plugin=org.kde.plasma.taskmanager
+
+[Containments][1][Applets][5]
+immutability=1
+plugin=org.kde.plasma.systemtray
+
+[Containments][1][Applets][5][Configuration][Containments][8]
+formfactor=2
+location=4
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][10]
+immutability=1
+plugin=org.kde.plasma.devicenotifier
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][11]
+immutability=1
+plugin=org.kde.plasma.notifications
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][12]
+immutability=1
+plugin=org.kde.plasma.clipboard
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][13]
+immutability=1
+plugin=org.kde.discovernotifier
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][14]
+immutability=1
+plugin=org.kde.plasma.battery
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][15]
+immutability=1
+plugin=org.kde.plasma.bluetooth
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][16]
+immutability=1
+plugin=org.kde.plasma.networkmanagement
+
+[Containments][1][Applets][5][Configuration][Containments][8][Applets][9]
+immutability=1
+plugin=org.kde.plasma.volume
+
+[Containments][1][Applets][5][Configuration][General]
+extraItems=org.kde.plasma.volume,org.kde.plasma.notifications,org.kde.discovernotifier,org.kde.plasma.mediacontroller,org.kde.plasma.battery,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.networkmanagement,org.kde.plasma.devicenotifier
+knownItems=org.kde.plasma.volume,org.kde.plasma.notifications,org.kde.discovernotifier,org.kde.plasma.mediacontroller,org.kde.plasma.battery,org.kde.plasma.bluetooth,org.kde.plasma.clipboard,org.kde.plasma.networkmanagement,org.kde.plasma.devicenotifier
+
+[Containments][1][Applets][6]
+immutability=1
+plugin=org.kde.plasma.digitalclock
+
+[Containments][1][General]
+AppletOrder=2;3;4;5;6
+
+[Containments][7]
+activityId=782e3974-9f83-40bd-b1a4-165f60c3aa75
 formfactor=0
 immutability=1
-lastScreen=-1
+lastScreen=0
 location=0
-plugin=org.kde.plasma.folder
+plugin=org.kde.desktopcontainment
 wallpaperplugin=org.kde.image
 
-[Containments][38][ConfigDialog]
-DialogHeight=480
-DialogWidth=640
-
-[Containments][38][Wallpaper][org.kde.image][General]
-Image=file:///usr/share/wallpapers/Auros/contents/images/5408x3464.png
+[Containments][7][Wallpaper][org.kde.image][General]
 height=768
 width=1366
 
-[General]
-immutability=1
 """
 
 def getMenuStyle(data):
-    regex = "(\[Containments\]\[[1-9]*\]\[Applets\]\[[1-9]*\]\nimmutability=.\nplugin=(.*)\n)"
+    regex = "(\[Containments\]\[([1-9])\]\[Applets\]\[([1-9])\]\nimmutability=1\nplugin=(.*)\n)"
 
     read = re.search(regex, data)
 
     if read:
-        return read.group(1), read.group(2)
+        return read.group(1), read.group(2), read.group(3), read.group(4)
 
-def setMenuStyle(data):
-    regex = "\[Containments\]\[[1-9]*\]\[Applets\]\[[1-9]*\]\nimmutability=.\nplugin=(.*)\n"
+def setMenuStyle(data, style):
+    regex = r"\[Containments\]\[%s\]\[Applets\]\[%s\]\nimmutability=.\nplugin=(.*)\n"%(getMenuStyle(data)[1],
+                                                                                                 getMenuStyle(data)[2])
 
     com = re.compile(regex)
 
-    return com.sub(getMenuStyle(data)[0].replace(getMenuStyle(data)[1], "org.kde.plasma.kicker"), data)
+    return com.sub(getMenuStyle(data)[0].replace(getMenuStyle(data)[3], style), data)
 
-def getDesktopStyle(data):
+def getDesktopView(data):
     regex = "(\[Containments\]\[[1-9]*\]\nactivityId=.+\n.*\n.*\nlastScreen=.*\n.*\nplugin=(.*)\n)"
 
     read = re.search(regex, data)
@@ -72,15 +148,51 @@ def getDesktopStyle(data):
     if read:
         return read.group(1), read.group(2)
 
-def setDesktopStyle(data):
+def setDesktopView(data, view):
     regex = "\[Containments\]\[[1-9]*\]\nactivityId=.+\n.*\n.*\nlastScreen=.*\n.*\nplugin=(.*)\n"
 
     com = re.compile(regex)
 
-    return com.sub(getDesktopStyle(data)[0].replace(getDesktopStyle(data)[1], "org.kde.plasma.desktop"), data)
+    return com.sub(getDesktopView(data)[0].replace(getDesktopView(data)[1], view), data)
+
+
+def getWallpaperGroup(data):
+    regex = "(\[Containments\]\[[1-9]+\]\[Wallpaper\]\[org.kde.image\]\[General\]\n(.*)=(.*)\n)"
+
+    read = re.search(regex, data)
+
+    if read:
+        if read.group(2) != "Image":
+            regex = "(\[Containments\]\[[1-9]+\]\[Wallpaper\]\[org.kde.image\]\[General\]\n)((.*)=.*\n.*\n)"
+
+            read = re.search(regex, data)
+
+            if read:
+                return read.group(1), read.group(2), read.group(3)
+
+        return read.group(1), read.group(2), read.group(3)
+
+
+def setWallpaperGroup(data, path):
+    if getWallpaperGroup(data)[1] != "Image":
+        regex = "\[Containments\]\[[1-9]+\]\[Wallpaper\]\[org.kde.image\]\[General\]\n.*\n.*\n"
+        com = re.compile(regex)
+
+        return com.sub((getWallpaperGroup(data)[0]+"Image=%s\n"+getWallpaperGroup(data)[1])%path, data)
+    else:
+        regex = r"\[Containments\]\[[1-9]+\]\[Wallpaper\]\[org.kde.image\]\[General\]\n%s=.*\n"%(getWallpaperGroup(data)[1])
+
+        com = re.compile(regex)
+
+        return com.sub(getWallpaperGroup(data)[0].replace(getWallpaperGroup(data)[2], path), data)
+
+
+
 
 
 #print(getMenuStyle(data))
 #print(setMenuStyle(data))
 #print(getDesktopStyle(data))
 #print(setDesktopStyle(data))
+#print(getWallpaperGroup(data))
+#print(setWallpaperGroup(data, "met2ehan.png"))
