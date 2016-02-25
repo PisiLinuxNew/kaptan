c3f2e7e359696dd3bf44997b394ddbd58ed35fda
mthnzbk <mthnzbk@gmail.com>
2016-02-25 08:40:44 +0200

    kurulum betiği oluşturuldu. kaptanın desktop dosyası halledildi ve kurulumda ilgili yerlere kopyalanması kurulum betiğinde belirtildi. script dosyası kurulum sonucuna göre düzenlendi.

--- setup.py
+++ setup.py
@@ -1,23 +1,64 @@
 from setuptools import setup, find_packages
+from distutils.command.install import install
+import os, shutil
+
+class Install(install):
+    def run(self):
+        #os.system("/setup.py build")
+
+        dirPath = os.path.dirname(__file__)
+
+        autostart_dir = "/usr/share/autostart"
+        project_dir = "/usr/share/kaptan"
+        pixmap_dir = "/usr/share/pixmaps"
+        applications_dir = "/usr/share/applications"
+        icon = os.path.join(dirPath, "data", "images", "kaptan-icon.png")
+
+        try:
+            os.makedirs(autostart_dir)
+            os.makedirs(project_dir)
+        except OSError:
+            print("He de geç...")
+
+
+        shutil.copy(os.path.join(dirPath, "data", "kaptan.desktop"), autostart_dir)
+        shutil.copy(os.path.join(dirPath, "data", "kaptan.desktop"), applications_dir)
+        shutil.copy(icon, os.path.join(pixmap_dir, "kaptan.png"))
+        shutil.copy("kaptan.py", os.path.join(project_dir, "kaptan5.py"))
+        shutil.copy("rc_kaptan.py", project_dir)
+
+        os.chmod("script/kaptan",0o755)
+        shutil.copy("script/kaptan", "/usr/bin")
+
+        os.system("cp -R languages {}".format(project_dir))
+        os.system("cp -R data {}".format(project_dir))
+        os.system("cp -R kaptan {}".format(project_dir))
+
+        docs = ["AUTHORS", "kaptan.pro", "kaptan.qrc", "LICENSE", "MANIFEST.in", "README", "TODO"]
+
+        for doc in docs:
+            shutil.copy(doc, project_dir)
+
+
 
 setup(
     name = "kaptan",
     packages = find_packages(),
-    #package_data = {"kaptan" : ["languages/*", "images/icons/*" ,"images/*.png", "media/*"]},
-    scripts = ["kaptan"],
+    package_data = {os.path.dirname(__file__) : ["languages/*", "data/images/*" ,"data/*"]},
+    scripts = ["script/kaptan"],
     version = "1.0",
     license = "GPL v3",
-    #description = "PisiLinux başlangıç yapılandırması.",
+    description = "PisiLinux desktop configurate.",
     author = "Metehan Özbek",
     author_email = "mthnzbk@gmail.com",
-    url = "https://github.com/mthnzbk/aptan",
+    url = "https://github.com/mthnzbk/kaptan",
     download_url = "https://pypi.python.org/pypi/kaptan",
-    #install_requires=["", ""],
     keywords = ["PyQt5"],
     classifiers = [
         "Environment :: X11 Applications :: Qt",
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: GNU Affero General Public License v3",
     ],
+    cmdclass = {'install': Install}
 
 )
